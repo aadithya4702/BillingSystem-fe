fe10206e5df6c856b5293b6a62e55fefbab809f4
aadithya4702 <aadithyaprasad111@gmail.com>
2025-04-06 19:15:23 +0530

    make cards to display 2 cols for tab view

--- src/pageSections/OrderSection.jsx
+++ src/pageSections/OrderSection.jsx
@@ -264,7 +264,7 @@ const OrderSection = () => {
           </div>
         </div>
 
-        <div className="pl-6 grid grid-cols-2 pr-6 relative sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6">
+        <div className="pl-6 pr-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredProducts.map((product) => {
             const cartItem = cart.find((item) => item.id === product.id);
 
